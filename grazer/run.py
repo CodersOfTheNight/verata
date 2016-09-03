89c6d2fda35cb6e9c646ed95515aea290cdc0b76
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-09-03 12:17:07 +0300

    Just output to console

--- grazer/run.py
+++ grazer/run.py
@@ -2,6 +2,7 @@ import click
 
 from dotenv import load_dotenv, find_dotenv
 from grazer.config import Config
+from grazer.core import crawler
 
 
 @click.command()
@@ -10,6 +11,8 @@ from grazer.config import Config
 def main(env, config):
     load_dotenv(env)
     cfg = Config(config)
+    for record, link in crawler.create(cfg):
+        print(record)
 
 if __name__ == "__main__":
     main()
