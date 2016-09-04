6c96139aa5b64dd264be2d52d0bbbbab66b74ba6
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-09-04 10:41:15 +0300

    Improving coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,13 @@
 import os
 import pytest
 
-from grazer.config import Config, Page, Mapping
+from grazer.config import Config, Page, Mapping, Auth
 from bs4 import BeautifulSoup
+
+
+@pytest.fixture
+def simple_config():
+    return Config("tests/data/simple_config.yml")
 
 
 def test_loading_existing():
@@ -52,3 +57,54 @@ def test_mapping():
     expected = [("link1", root.find("a", {"id": "link1"}).text)]
 
     assert result == expected
+
+
+class TestConfig(object):
+
+    def test_domain(self, simple_config):
+        domain = simple_config.domain
+        assert domain == "test.com"
+
+    def test_unparsable_domain(self, simple_config):
+        cfg = simple_config
+        cfg._data["site_root"] = "spd://test.com"
+        domain = cfg.domain
+        assert domain == "spd://test.com"
+
+    def test_empty_headers(self, simple_config):
+        assert simple_config.headers is None
+
+    def test_headers(self, simple_config):
+        cfg = simple_config
+        cfg._data["headers"] = {"test": "1"}
+        assert cfg.headers == {"test": "1"}
+
+    def test_empty_cookies(self, simple_config):
+        assert simple_config.cookies is None
+
+    def test_cookies(self, simple_config):
+        cfg = simple_config
+        cfg._data["cookies"] = {"test": "1"}
+        assert cfg.cookies == {"test": "1"}
+
+
+class TestAuth(object):
+
+    def test_default_method(self):
+        auth = Auth({})
+        assert auth.method == "POST"
+
+    def test_custom_method(self):
+        auth = Auth({"method": "PATCH"})
+        assert auth.method == "PATCH"
+
+    def test_url(self):
+        auth = Auth({"url": "/login"})
+        assert auth.url == "/login"
+
+    def test_params(self):
+        auth = Auth({"params": {"user": "test_user",
+                                "password": "test_passw"}
+                     })
+        assert auth.params["user"] == "test_user"
+        assert auth.params["password"] == "test_passw"
