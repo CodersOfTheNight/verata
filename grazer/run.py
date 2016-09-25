df25af8c12f824ee46a7bbf676f9adfcef5b1624
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-09-25 09:33:42 +0300

    Allow to config log level

--- grazer/run.py
+++ grazer/run.py
@@ -1,4 +1,5 @@
 import click
+import logging
 
 from dotenv import load_dotenv, find_dotenv
 from grazer.config import Config
@@ -8,7 +9,9 @@ from grazer.core import crawler
 @click.command()
 @click.option("--env", default=find_dotenv())
 @click.option("--config")
-def main(env, config):
+@click.option("--log_level", default="INFO")
+def main(env, config, log_level):
+    logging.basicConfig(level=getattr(logging, log_level))
     load_dotenv(env)
     cfg = Config(config)
     for record, link in crawler.create(cfg):
