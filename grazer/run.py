691eaa781aadc15ac4d956d109fe5736ad46536d
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-12-26 14:00:17 +0200

    More verbose output

--- grazer/run.py
+++ grazer/run.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv, find_dotenv
 from grazer.config import Config
 from grazer.core import crawler
 
+logger = logging.getLogger("Verata")
+
 
 @click.command()
 @click.option("--env", default=find_dotenv())
@@ -22,6 +24,7 @@ def main(env, config, log_level, debug, output):
 
     with open(output, "w") as f:
         for record, link in crawler.create(cfg):
+            logging.debug("Record: {0} Link: {1}".format(record, link))
             f.write("({0}, {1})\n".format(record, link))
 
 if __name__ == "__main__":
