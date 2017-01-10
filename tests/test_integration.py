eabbdec6da04da8de8e59fcebc8f1f70778215b3
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-01-10 20:33:16 +0200

    Rest of impl

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,26 @@
 import logging
 
-from grazer.core import crawler
+from grazer.core import crawler, scrapper
 from grazer.config import Config
 
 logging.basicConfig()
 
 
-def test_scrape_python_org():
+def test_crawl_python_org():
     cfg = Config("tests/data/python_org.yml")
     worker = crawler.create(cfg)
     data, link = next(worker)
+    print(data)
     (key, text, attrs) = data[0]
+    assert key == "title"
+
+
+def test_scrape_python_org():
+    cfg = Config("tests/data/python_org.yml")
+    data = scrapper.fetch_page("http://python.org/blogs/", cfg)
+    results = scrapper.scrape(data, cfg.mappings)
+    print(results)
+    (key, text, attrs) = results[0]
     assert key == "title"
 
 
---
+++ tests/test_parsing.py
@@ -0,0 +1,44 @@
+from pytest import fixture
+from bs4 import BeautifulSoup
+
+from grazer.config import Mapping
+
+
+@fixture
+def example_html():
+    html = """
+        <html>
+            <body>
+                <h1>Menu:</h1>
+                <ul>
+                    <li>
+                        <a href=#>Nested Link</a>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>
+                                <a href=#>Incorrect</a>
+                            </li>
+                            <li class="deep">
+                                <a href=#>Deepest one</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </body>
+        </html>
+    """
+    return BeautifulSoup(html)
+
+
+def test_0_depth(example_html):
+    mapping = Mapping("title", "h1")
+    result = mapping.parse(example_html)
+    assert len(result) == 1
+    assert result[0] == ("title", "Menu:", {})
+
+
+def test_2_depth(example_html):
+    mapping = Mapping("link", "ul/li/a")
+    result = mapping.parse(example_html)
+    assert result[0] == ("link", "Nested Link", {"href": "#"})
